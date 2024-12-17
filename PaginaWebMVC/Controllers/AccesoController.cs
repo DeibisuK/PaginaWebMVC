3b9329b0533229b8d5b632628cf48f02f99721d4
DeibisuK <jhongamer530@gmail.com>
2024-12-17 13:25:03 -0500

    Agregar archivos de proyecto.

---
+++ PaginaWebMVC/Controllers/AccesoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaginaWebMVC.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PaginaWebMVC.Controllers
+{
+    public class AccesoController : Controller
+    {
+        static string cadena = "Data Source=´MACHINEGUN\\SQLEX´;initial catalog=PruebaLogin;integrated security=true";
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Register(Usuario user)
+        {
+            bool registrado;
+            string mensaje;
+
+            if (user.Contraseña == user.ConfirmarClave)
+            {
+                user.Contraseña = user.ConfirmarClave;
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Las constraseñas no coinciden";
+                return View();
+            }
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("SP_AgregarUsuario",cn);
+                cmd.Parameters.AddWithValue("Nombre",user.Nombre);
+                cmd.Parameters.AddWithValue("Email",user.Email);
+                cmd.Parameters.AddWithValue("Contraseña",user.Contraseña);
+            }
+
+                return View();
+        }
+    }
+}
---
+++ PaginaWebMVC/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaginaWebMVC.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public string ConfirmarClave { get; set; }
+
+    }
+}
