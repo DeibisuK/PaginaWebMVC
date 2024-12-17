d397bee47a711472d245edb22fcce75eddf351c4
DeibisuK <jhongamer530@gmail.com>
2024-12-17 13:51:30 -0500

    Quite el register del controlador

--- PaginaWebMVC/Controllers/AccesoController.cs
+++ PaginaWebMVC/Controllers/AccesoController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using PaginaWebMVC.Models;
 using System.Data;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Services.Description;
 
 namespace PaginaWebMVC.Controllers
 {
@@ -17,11 +20,36 @@ namespace PaginaWebMVC.Controllers
         {
             return View();
         }
-        public ActionResult Register()
-        {
-            return View();
-        }
 
+        [HttpPost]
+        public ActionResult Login(Usuario user)
+        {
+            user.Contraseña = ConvertirSha256(user.Contraseña);
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("SP_ValidarUsuario", cn);
+                cmd.Parameters.AddWithValue("Email", user.Email);
+                cmd.Parameters.AddWithValue("Contraseña", user.Contraseña);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+                user.Id = Convert.ToInt32( cmd.ExecuteScalar().ToString());
+            }
+
+            if (user.Id != 0)
+            {
+                Session["usuario"] = user;
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewData["Mensaje"] = "usuario no encontrado";
+                return View();
+            }
+
+           
+        }
 
         [HttpPost]
         public ActionResult Register(Usuario user)
@@ -31,7 +59,7 @@ namespace PaginaWebMVC.Controllers
 
             if (user.Contraseña == user.ConfirmarClave)
             {
-                user.Contraseña = user.ConfirmarClave;
+                user.Contraseña = ConvertirSha256(user.Contraseña);
             }
             else
             {
@@ -45,9 +73,46 @@ namespace PaginaWebMVC.Controllers
                 cmd.Parameters.AddWithValue("Nombre",user.Nombre);
                 cmd.Parameters.AddWithValue("Email",user.Email);
                 cmd.Parameters.AddWithValue("Contraseña",user.Contraseña);
+                cmd.Parameters.Add("Registro",SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("Mensaje",SqlDbType.VarChar,100).Direction = ParameterDirection.Output;
+                cmd.CommandType = CommandType.StoredProcedure;
+                
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                registrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
+                mensaje = cmd.Parameters["Registrado"].Value.ToString();
             }
 
+            ViewData["Mensaje"] = mensaje;
+            
+            if (registrado)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
                 return View();
+            }
         }
+
+        public static string ConvertirSha256(string texto)
+        {
+            //using System.Text;
+            //USAR LA REFERENCIA DE "System.Security.Cryptography"
+
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
     }
 }
